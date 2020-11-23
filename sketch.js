1e7ab3771af21bf2fdd3f17709e47744e5170ccb
Harmanveer <harmanveer@gmail.com>
2020-11-23 15:56:50 +0530

    started lucas

---
+++ sketch.js
@@ -0,0 +1,19 @@
+var lucas;
+
+
+function preload()
+{
+
+}
+
+function setup()
+{
+    createCanvas(500,800);
+    lucas = createSprite(250,400,20,20);
+}
+
+function draw()
+{
+    background("red");
+    drawSprites();
+}
